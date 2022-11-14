42c301d6f561e8c6a1174e06c83912efd3a7183c
Sergey Vyalov <s.vyalov@gmail.com>
2022-11-14 18:21:16 +0400

    Init commit

---
+++ force-app/main/default/lwc/app/app.js
@@ -0,0 +1,7 @@
+import { LightningElement } from "lwc";
+export default class App extends LightningElement {
+  bike = {
+    name: "Electra X4",
+    picture: "https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg"
+  };
+}
---
+++ force-app/main/default/lwc/bike/bike.js
@@ -0,0 +1,4 @@
+import { LightningElement, api } from "lwc";
+export default class Bike extends LightningElement {
+  @api bike;
+}
